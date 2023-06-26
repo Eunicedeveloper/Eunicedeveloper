4df963a82e22c416cc98d7debfd47c4c854afd66
eMobilis <eunycewang@gmail.com>
2023-06-26 09:25:35 +0300

    simple steps to calculate area using python

--- calc-area.py
+++ calc-area.py
@@ -1,10 +1,11 @@
 #enable user to enter input
 length = input(" enter length")
-width = input( " enter width")
+width = input("enter width")
 
 # convert two variable into integer
-length = int(length )
-width = int( width)
+length = int(length)
+width = int(width)
 
-#calculation
+#calculating area of a rectangle
 result = length * width
+print("Area is",result)
